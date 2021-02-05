d117791de83e71c3ddcdddd2161839270f320796
barbaramachado.dex@gmail.com <barbaramachado.dex@gmail.com>
2021-02-05 15:37:58 -0800

    Code challenge for 02/04/2021.
    Alternative faster answer:
    class Solution:
        def threeConsecutiveOdds(self, arr: List[int]) -> bool:
            for i in range (len(arr)):
                try:
                    if arr[i]% 2 != 0 and arr[i+1]%2 !=0 and arr[i+2]%2 !=0:
                        return True
                except Exception:
                    pass
    
            return False

---
+++ Feb_2021/20210205.py
@@ -0,0 +1,19 @@
+"""
+1672. Richest Customer Wealth
+
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+"""
+
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        valWealth = -inf
+        for l in accounts:
+            sumWealth = sum(l)
+            # print(sumWealth)
+            if sumWealth > valWealth:
+                valWealth = sumWealth
+
+        return valWealth
