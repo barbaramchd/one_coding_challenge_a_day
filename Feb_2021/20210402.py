4596d00fd2e925fcfdd5009a0973c04b4c01f8a1
barbaramachado.dex@gmail.com <barbaramachado.dex@gmail.com>
2021-02-04 23:22:42 -0800

    Code challenge for 02/04/2021.
    Alternative faster answer:
    class Solution:
        def threeConsecutiveOdds(self, arr: List[int]) -> bool:
            for i in range (len(arr)):
                try:
                    if arr[i]% 2 != 0 and arr[i+1]%2 !=0 and arr[i+2]%2 !=0:
                        return True
                except Exception:
                    pass
    
            return False

---
+++ Feb_2021/20210402.py
@@ -0,0 +1,17 @@
+"""
+1550. Three Consecutive Odds
+
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+"""
+
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        for i in range(len(arr)):
+            if len(arr[i + 1:]) >= 2:
+                print("a: ", arr[i])
+                print("b:", arr[i + 1])
+                print("c:", arr[i + 2])
+                if arr[i] % 2 != 0 and arr[i + 1] % 2 != 0 and arr[i + 2] % 2 != 0:
+                    return True
+        return False
