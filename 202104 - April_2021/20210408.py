390174d6dbc3a4960674fa6098f9ef200176e78f
barbaramachado.dex@gmail.com <barbaramachado.dex@gmail.com>
2021-04-08 23:58:18 -0700

    Code challenge for 04/08/2021.

---
+++ 202104 - April_2021/20210408.py
@@ -0,0 +1,35 @@
+"""
+Letter Combinations of a Phone Number
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+"""
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        digit_to_letter_dict = {
+            "2": ['a', 'b', 'c'],
+            "3": ['d', 'e', 'f'],
+            "4": ['g', 'h', 'i'],
+            "5": ['j', 'k', 'l'],
+            "6": ['m', 'n', 'o'],
+            "7": ['p', 'q', 'r', 's'],
+            "8": ['t', 'u', 'v'],
+            "9": ['w', 'x', 'y', 'z']
+        }
+        combinations = []
+        for index in range(len(digits) - 1):
+            for value1 in digit_to_letter_dict[digits[index]]:
+                combinations2 = []
+                if len(combinations) == 0:
+                    combinations = digit_to_letter_dict[digits[index]]
+                for value2 in range(len(combinations)):
+                    comb = value1 + combinations[value2]
+                    combinations2.append(comb)
+
+                combinations = combinations2
+
+        return combinations
