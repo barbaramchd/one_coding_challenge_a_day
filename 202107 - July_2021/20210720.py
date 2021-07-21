ed327371fdbd09cedd6e9cb02a782030b5d1f311
barbaramachado.dex@gmail.com <barbaramachado.dex@gmail.com>
2021-07-20 23:09:26 -0700

    Code challenge for 07/20/2021.

---
+++ 202107 - July_2021/20210720.py
@@ -0,0 +1,32 @@
+"""
+  Shuffle an Array
+
+Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+
+Implement the Solution class:
+
+Solution(int[] nums) Initializes the object with the integer array nums.
+int[] reset() Resets the array to its original configuration and returns it.
+int[] shuffle() Returns a random shuffling of the array.
+"""
+
+import random
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.nums_copy = nums.copy()
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.nums
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        random.shuffle(self.nums_copy)
+        return self.nums_copy
